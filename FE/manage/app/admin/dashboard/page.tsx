c127ca7c8ccc567a0de6462781e527bb171ae4b2
Anh Tuan Vu <tuanvahe2021@gmail.com>
2025-06-10 05:20:50 +0700

    Mau chuc nang login, chia tach code, loi login voi gooogle chua sua

---
+++ FE/manage/app/admin/dashboard/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import Header from '@/components/common/Header';
+// Components
+
+export default function Dashboard() {
+
+ return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <div className="max-w-7xl mx-auto p-8">
+        <p>hihi</p>
+      </div>
+    </div>
+  );
+}
---
+++ FE/manage/src/services/authService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://localhost:7074/api";
+
+export interface LoginData {
+  username: string;
+  password: string;
+  recaptchaToken: string;
+}
+
+export const login = async (data: LoginData) => {
+  const response = await axios.post(`${API_URL}/Auth/login`, data);
+  return response.data;
+};
+
+export interface LoginGoogleData {
+  idToken: string;
+}
+export const loginGoogle = async (data: LoginGoogleData) => {
+  const response = await axios.post(`${API_URL}/Auth/login-google`, data);
+  return response.data;
+};
