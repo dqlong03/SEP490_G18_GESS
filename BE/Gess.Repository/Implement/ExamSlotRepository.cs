3c1dca48f36e062cdf25f7492a46901b2f28676c
anhtuansan <tuanvahe2021@gmail.com>
2025-08-19 22:29:25 +0700

    update

--- BE/Gess.Repository/Implement/ExamSlotRepository.cs
+++ BE/Gess.Repository/Implement/ExamSlotRepository.cs
@@ -830,7 +830,7 @@ namespace GESS.Repository.Implement
                     StartTime = item.StartTime.ToLocalTime().TimeOfDay,
                     EndTime = item.EndTime.ToLocalTime().TimeOfDay,
                     SlotName = item.SlotName,
-                    ExamDate = item.Date,
+                    ExamDate = item.Date.ToLocalTime().Date,
                     MultiOrPractice = item.MultiOrPractice,
                     Status = string.IsNullOrEmpty(item.Status) ? "Chưa gán bài thi" : item.Status,
                     SubjectId = item.SubjectId,
@@ -847,7 +847,7 @@ namespace GESS.Repository.Implement
                     SemesterId = item.SemesterId,
                     SubjectId = item.SubjectId,
                     MultiOrPractice = item.MultiOrPractice,
-                    ExamDate = item.Date,
+                    ExamDate = item.Date.ToLocalTime().Date,
                     IsGraded = 0,
                     Status = 0
                 }).ToList();
